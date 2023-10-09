e86fc753e794595ed566cf6343beefeae9a5d218
Mayya Markova <mayya.markova@gmail.com>
2023-10-09 15:09:45 +0300

    Add purchase form

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { CartService } from '../cart.service';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-cart',
@@ -9,7 +10,19 @@ import { CartService } from '../cart.service';
 export class CartComponent {
   items = this.cartService.getCart();
 
+  checkoutFrom = this.formBuilder.group({
+    name: '',
+    address: '',
+  });
+
   constructor(
-    private cartService: CartService
+    private cartService: CartService,
+    private formBuilder: FormBuilder,
   ) { }
+
+  onSubmit(): void {
+    this.items = this.cartService.clearCart();
+    console.warn('The order was submitted!', this.checkoutFrom.value);
+    this.checkoutFrom.reset();
+  }
 }

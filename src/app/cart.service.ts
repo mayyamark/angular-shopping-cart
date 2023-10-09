b274340d8b9bb752ddcc63872c0b3d7b34530a42
Mayya Markova <mayya.markova@gmail.com>
2023-10-09 13:29:32 +0300

    Implement CartService

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { Product } from './products';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
+  items: Product[] = [];
 
   constructor() { }
+
+  getCart() {
+    return this.items;
+  }
+
+  addToCart(product: Product) {
+    this.items.push(product);
+  }
+
+  clearCart() {
+    this.items = [];
+    return this.items;
+  }
 }
